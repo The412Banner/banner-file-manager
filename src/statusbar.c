bdcadaa478546597dad2bb4c50caa697dc7ce5c5
The412Banner <205237651+The412Banner@users.noreply.github.com>
2026-07-20 16:59:54 -0400

    Banner File Manager: dark status bar (owner-drawn)
    The bottom status bar was the last light strip (Wine renders msctls_statusbar32
    light). Owner-draw it dark: subclass, WM_PAINT fills RGB(45,45,45) and draws the
    item-count/size text in light; setStatusbarText now stores + invalidates.

--- src/statusbar.c
+++ src/statusbar.c
@@ -5,12 +5,45 @@ extern HWND hwndMain;
 
 HWND hwndStatusbar = NULL;
 
+// Wine renders the status bar with a light background; owner-draw it dark to match.
+static WNDPROC OrigStatusProc = NULL;
+static wchar_t statusText[160] = {0};
+
+static LRESULT CALLBACK StatusWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_ERASEBKGND) return 1;
+    if (msg == WM_PAINT) {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        RECT rc;
+        GetClientRect(hwnd, &rc);
+
+        HBRUSH bg = CreateSolidBrush(RGB(45, 45, 45));
+        FillRect(hdc, &rc, bg);
+        DeleteObject(bg);
+
+        HGDIOBJ oldFont = SelectObject(hdc, getUIFont());
+        SetBkMode(hdc, TRANSPARENT);
+        SetTextColor(hdc, RGB(220, 220, 220));
+        RECT tr = rc;
+        tr.left += 8;
+        DrawText(hdc, statusText, -1, &tr, DT_LEFT | DT_VCENTER | DT_SINGLELINE | DT_END_ELLIPSIS);
+        SelectObject(hdc, oldFont);
+
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    return CallWindowProc(OrigStatusProc, hwnd, msg, wParam, lParam);
+}
+
 void setStatusbarText(wchar_t* text) {
-    SetWindowText(hwndStatusbar, text); 
+    wcscpy_s(statusText, 160, text ? text : L"");
+    InvalidateRect(hwndStatusbar, NULL, TRUE);
 }
 
 void createStatusbar() {
-    hwndStatusbar = CreateWindowEx(0, STATUSCLASSNAME, NULL, WS_VISIBLE | WS_CHILD | WS_CLIPSIBLINGS, 
+    hwndStatusbar = CreateWindowEx(0, STATUSCLASSNAME, NULL, WS_VISIBLE | WS_CHILD | WS_CLIPSIBLINGS,
                                    0, 0, 0, 0, hwndMain, (HMENU)NULL, globalHInstance, NULL);
+    SendMessage(hwndStatusbar, WM_SETFONT, (WPARAM)getUIFont(), TRUE);
+    OrigStatusProc = (WNDPROC)SetWindowLongPtr(hwndStatusbar, GWLP_WNDPROC, (LONG_PTR)StatusWndProc);
     UpdateWindow(hwndStatusbar);
 }
